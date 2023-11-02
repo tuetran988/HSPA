df6f02d422609f3242a90afb16c213011ee144f6
tuetran <tuemeoptit@gmail.com>
2023-11-02 22:08:35 +0700

    service

--- FE/src/app/property/property-list/property-list.component.ts
+++ FE/src/app/property/property-list/property-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HousingService } from 'src/app/services/housing.service';
 
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
-  styleUrls: ['./property-list.component.css']
+  styleUrls: ['./property-list.component.css'],
 })
-export class PropertyListComponent {
-
+export class PropertyListComponent implements OnInit{
+  Properties:any = [] ;
+  constructor(private housingService : HousingService){}
+  ngOnInit():void {
+     this.housingService.getAllProperties().subscribe(
+        data => {
+          this.Properties = data
+        } ,
+        error=>{
+          console.log(error);
+        }
+     )
+  }
 }
